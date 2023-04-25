088b13d6a27f50d728cecb5c4d86898d772d99ac
David <davferrufino11@gmail.com>
2023-04-25 14:57:33 -0400

    Created packer and canvas completed

--- Assignment7.py
+++ Assignment7.py
@@ -2,14 +2,22 @@ import sys
 from sys import argv
 import shapely
 import tkinter as tk
-from rectpack import newPacker
+from rectpack import newPacker, PackingMode, PackingBin, SORT_AREA
 class CustomCanvas:
     #assigning parameters for custom canvas using tk
     def __init__(self, height: int, width: int):
         self.root = tk.Tk()
-        self.canvas = tk.Canvas(height = height, width = width)
+        self.root.title("testCanvas")
+        self.canvas = tk.Canvas(self.root, height = height, width = width)
         self.canvas.pack()
+
+
+    def displayRects(self, rectangles):
+        for shape in rectangles:
+            self.canvas.create_rectangle(shape.x, shape.y, shape.x + shape.width, shape.y + shape.height, fill='blue',
+                                    outline='black')
         self.root.mainloop()
+
 
 class Rectangle:
     def __init__(self, height: int, width: int, x: int = 0, y: int = 0):
@@ -17,28 +25,34 @@ class Rectangle:
         self.width = width
         self.x = x
         self.y = y
-def pack(allRec: list, canvasSize: int):
+def pack(allRec, canvasSize):
   #Creating new packer object
-  shapePacker = newPacker()
+  height, width = canvasSize
+  shapePacker = newPacker(mode=PackingMode.Offline, bin_algo=PackingBin.BNF, sort_algo=SORT_AREA, rotation=False)
 
   for shape in allRec:
     #adding each rectangle and its paramters
-    shapePacker.add_rect(shape.width, shape.height, shape)
+    shapePacker.add_rect(shape.width, shape.height, rid=shape)
 
+  shapePacker.add_bin(width, height)
   shapePacker.pack()
 
-  packedRects = []
+  #packedRects = []
+  #Assining the x and y coordinates for each shape
+  #packer will put the rect at shape[5] for each iterable
+  #so we must go to shape[5] to modify rect atrributes
   for shape in shapePacker.rect_list():
-    x, y = shape.x, shape.y
-    #creating a new Rectangle object, and appending it to packedRects
-    packedRects.append(Rectangle(shape.width, shape.height,x, y))
-  return packedRects
+    shape[5].x = shape[1]
+    shape[5].y = shape[2]
+        #creating a new Rectangle object, and appending it to packedRects
+#    packedRects.append(Rectangle(shape.width, shape.height, x, y))
+  return allRec
 
 def main():
   file = sys.argv[1] #file path
   with open(file, 'r') as f:
       #since it is int,int: splitting with the , results in tuple (int int)
-      canvasParam = tuple(f.readline().split(',')) #first line in file will be canvas args
+      canvasParam = tuple(map(int, f.readline().split(','))) #first line in file will be canvas args
       recsize = [tuple(map(int, line.split(','))) for line in f] #each line is going to be an arg for a rect
       #so I split the rec, store the args in a list of tuples so i can iterate thru it later
 
@@ -47,6 +61,14 @@ def main():
 
   #iterating thru all of our rectangle arguments that we tupled
   rectangles = [Rectangle(*arguments) for arguments in recsize]
+
+  packedRects = pack(rectangles, canvasParam)
+
+  #for shape in packedRects:
+  #    myCanvas.canvas.create_rectangle(shape.x, shape.x, shape.x+shape.width, shape.y+shape.height, fill='blue',outline='black')
+
+  myCanvas.displayRects(packedRects)
+
 
 if __name__ == '__main__':
   main()
